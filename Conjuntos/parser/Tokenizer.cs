54aed3af002200331a389a905df986bb99b647d0
gmbarrera <gmbarrera@gmail.com>
2017-09-20 19:20:08 -0300

    Extract Token class

---
+++ Conjuntos/parser/Tokenizer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Conjuntos.parser
+{
+    public class Tokenizer
+    {
+        private List<TokenInfo> tokenInfos;
+        private List<Token> tokens;
+
+        public Tokenizer()
+        {
+            tokenInfos = new List<TokenInfo>();
+            tokens = new List<Token>();
+        }
+
+        class TokenInfo
+        {
+            public Regex regex;
+            public int token;
+
+            public TokenInfo(Regex regex, int token)
+            {
+                this.regex = regex;
+                this.token = token;
+            }
+        }
+        
+        public void Add(String regex, int token)
+        {
+            tokenInfos.Add(new TokenInfo(new Regex("^(" + regex + ")"), token));
+        }
+
+        public void Tokenize(String str)
+        {
+            String s = str.Trim();
+            tokens.Clear();
+            while (!s.Equals(""))
+            {
+                bool match = false;
+                foreach (TokenInfo info in tokenInfos)
+                {
+                    var m = info.regex.Match(s);
+                    if (m.Success)
+                    {
+                        match = true;
+                        String tok = m.Value.Trim();
+                        s = s.Remove(m.Index, m.Length).Trim();
+                        tokens.Add(new Token(info.token, tok));
+                        break;
+                    }
+                }
+                if (!match) throw new Exception("Unexpected character in input: " + s);
+            }
+        }
+        public List<Token> GetTokens()
+        {
+            return tokens;
+        }
+    }
+    
+}
